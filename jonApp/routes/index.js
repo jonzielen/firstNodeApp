936674773d67c142bc0fee18ea001bdd472242f1
Jon Zielenkievicz <jzielenkievicz@nycgo.com>
2015-05-28 14:26:12 -0400

    add normalize to scss

--- jonApp/routes/index.js
+++ jonApp/routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
+// get year
+var year = new Date();
+year = year.getFullYear()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
       title: 'The best website ever!',
       jom: 'jom',
-      second: 'test 33333'
+      second: 'test 33333',
+      year: year
      });
 });
 
